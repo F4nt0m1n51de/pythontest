e8bc36520c9d496a962fdac3fad49e51e7de705e
F4nt0m1n51de <semennikolaj5@gmail.com>
2025-11-29 16:50:49 +0500

    Calculator

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    if b != 0:
+        return a / b
+    return "Ошибка: деление на ноль!"
+if __name__ == "__main__":
+    print("Калькулятор запущен!")
+    x = int(input())
+    y = int(input())
+print(f"{x} + {y} = {add(x, y)}")
+print(f"{x} - {y} = {subtract(x, y)}")
+print(f"{x} * {y} = {multiply(x, y)}")
+print(f"{x} / {y} = {divide(x, y)}")
